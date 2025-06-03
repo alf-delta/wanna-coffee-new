154e863dadec962098de30dd8d80c352eb89a3f9
alf-delta <alf.delta727@gmail.com>
2025-06-03 14:51:12 -0400

    fix: оригинальный SVG Google Maps Pin для карточек кофеен

--- src/components/CoffeeList.jsx
+++ src/components/CoffeeList.jsx
@@ -37,7 +37,14 @@ const CoffeeList = ({ coffeeShops = [], onShopClick, horizontal = false, showCou
               <span role="img" aria-label="walk">🚶‍♂️</span>
               <span style={styles.arrow}>→</span>
               <span style={styles.gmapsIcon}>
-                <img src="/src/assets/google-maps-pin.svg" alt="Google Maps" width={20} height={20} />
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+                  <g>
+                    <path fill="#4285F4" d="M43.6 20.5h-1.9V20H24v8h11.3c-1.6 4.3-5.7 7-11.3 7-6.6 0-12-5.4-12-12s5.4-12 12-12c2.9 0 5.5 1 7.6 2.7l6-6C34.3 5.1 29.4 3 24 3 12.9 3 4 11.9 4 23s8.9 20 20 20c11 0 19.7-8 19.7-20 0-1.3-.1-2.7-.3-4z"/>
+                    <path fill="#34A853" d="M6.3 14.7l6.6 4.8C14.5 16.1 18.9 13 24 13c2.9 0 5.5 1 7.6 2.7l6-6C34.3 5.1 29.4 3 24 3 15.6 3 8.1 8.5 6.3 14.7z"/>
+                    <path fill="#FBBC05" d="M24 43c5.4 0 10.3-1.8 14.1-4.9l-6.5-5.3C29.5 34.9 26.9 36 24 36c-5.6 0-10.3-3.8-12-9l-6.6 5.1C8.1 39.5 15.6 45 24 45z"/>
+                    <path fill="#EA4335" d="M43.6 20.5h-1.9V20H24v8h11.3c-1.1 3-3.6 5.1-6.3 6.1l6.5 5.3C41.2 36.1 44 30.9 44 24c0-1.3-.1-2.7-.4-4z"/>
+                  </g>
+                </svg>
               </span>
             </button>
           </div>
